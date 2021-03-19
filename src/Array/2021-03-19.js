437ab895bd91c518bf2ce83b326df518d0fc68ad
chenbiao <chenbiao@zte-its.com>
2021-03-19 13:26:42 +0800

    代码优化

--- src/Array/2021-03-19.js
+++ src/Array/2021-03-19.js
@@ -13,9 +13,7 @@
  */
 var missingNumber = function(nums) {
     for(let i=0;i<nums.length;i++){
-        if(nums[i]!=i){
-            return i;
-        }
+        if(nums[i]!=i) return i;
     }
     return nums.length;
 };
