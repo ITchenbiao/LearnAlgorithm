4769e2a2884dbff6225ef0c7448748eb9a795977
chenbiao <chenbiao0828@foxmail.com>
2021-04-08 18:29:59 +0800

    add

--- src/String/2021-04-08.js
+++ src/String/2021-04-08.js
@@ -14,6 +14,7 @@
  * 2021-04-08
  */
  
+// method 1
  var longestPalindrome = function(s) {
     if(!s || s.length < 2){
         return s;
@@ -41,3 +42,29 @@
     }
     return s.substring(start,end+1);
 };
+
+// method 2
+var longestPalindrome = function(s) {
+    if (s.length === 1) return s;
+    let maxRes = 0, maxStr = '';
+    for (let i = 0; i < s.length; i++) {
+        let str1 = palindrome(s, i, i);
+        let str2 = palindrome(s, i, i + 1);   
+        if (str1.length > maxRes) {
+            maxStr = str1;
+            maxRes = str1.length;
+        }
+        if (str2.length > maxRes) {
+            maxStr = str2;
+            maxRes = str2.length;
+        }
+    }
+    return maxStr;
+};
+function palindrome(s, l, r) {
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+        l--;
+        r++;
+    }
+    return s.slice(l + 1, r);
+}
